b1ef5fff9f28952cc1fbf096070d6b4d76ac20bb
tutdeputraw <anoboy.code@gmail.com>
2022-05-07 13:01:44 +0700

    change the displayed text in the App.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Goodbye 
+          This is what it feels like!
         </a>
       </header>
     </div>
